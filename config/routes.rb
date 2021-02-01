b99733cdfa6f63ed444a2275cb23c38d1bb0c066
stanislas legrez <stanislas27@gmail.com>
2021-02-01 22:39:45 +0100

    added all CRUD, routes, some css, bootstrap gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  # get "movies", to: "movies#index"
+  # get "movies/new", to: "movies#new"
+  # post "movies", to: "movies#create"
+  # get "movies/:id", to: "movies#show", as: :movie
+  # get "movies/:id/edit", to: "movies#edit"
+  # patch "movies/:id", to: "movies#update"
+  # delete "movies/:id", to: "movies#destroy"
+
+  resources :movies
 end
