cc3cfbc0b58a6cc78aaf2828fd79caf213e1c001
stanislas legrez <stanislas27@gmail.com>
2021-02-04 12:37:42 +0100

    added review model + controller, migration, nested route

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @movie = Movie.find(params[:movie_id])
+    @review.movie = @movie
+    if @review.save
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to movie_path(@review.movie)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # get "movies", to: "movies#index"
-  # get "movies/new", to: "movies#new"
-  # post "movies", to: "movies#create"
-  # get "movies/:id", to: "movies#show", as: :movie
-  # get "movies/:id/edit", to: "movies#edit"
-  # patch "movies/:id", to: "movies#update"
-  # delete "movies/:id", to: "movies#destroy"
 
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [ :new, :create ]
+  end
+  
+  resources :reviews, only: [ :destroy ]
+
 end
