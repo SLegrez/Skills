4f3507800296ceb111fd562796f142180eccb70f
stanislas legrez <stanislas27@gmail.com>
2021-02-04 18:13:47 +0100

    added custom validations, some style

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
-  validates :title, :year, :rated, :duration, :genre, presence: true
+  validates :title, :year, :genre ,:duration, presence: true
+  validates :rated, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, presence: true
+  validates :duration, numericality: { greater_than_or_equal_to: 45, less_than_or_equal_to: 600 }, presence: true
 end
