a652b475a25ef28af8a988339996671a6dd45c0a
stanislas legrez <stanislas27@gmail.com>
2021-02-05 18:16:15 +0100

    api is working but not the search bar

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,29 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+const results = document.querySelector("#results");
+const searchMovies = (query) => {
+  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+  .then(response => response.json())
+  .then((data) => {
+    // console.log(data)
+    data.Search.forEach((result) => {
+        const movieList = `<li class="list-inline-item">
+          <img src="${result.Poster}" alt="">
+          <p>${result.Title}</p>
+          </li>`;
+          // console.log(movieList)
+
+        results.insertAdjacentHTML("beforeend", movieList);
+      });;
+    });
+
+const form = document.querySelector('#search-movies');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // <-- to prevent <form>'s native behaviour
+  const input = event.currentTarget.querySelector('.form-control');
+  results.innerHTML = '';
+  searchMovies(input.value);
+});
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   end
   resources :reviews, only: [ :destroy ]
 
+  get 'api', to: 'pages#api'
 end
