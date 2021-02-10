1209d8bd6c29f89bcde1b22358bd3da228e4f893
stanislas legrez <stanislas27@gmail.com>
2021-02-10 15:07:27 +0100

    api callbacks are working fine

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,28 +4,30 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
-const results = document.querySelector("#results");
-const searchMovies = (query) => {
-  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
-  .then(response => response.json())
-  .then((data) => {
-    // console.log(data)
-    data.Search.forEach((result) => {
-        const movieList = `<li class="list-inline-item">
-          <img src="${result.Poster}" alt="">
-          <p>${result.Title}</p>
-          </li>`;
-          // console.log(movieList)
+document.addEventListener("DOMContentLoaded", function() {
+  const results = document.querySelector("#results");
+  const searchMovies = (query) => {
+    fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+    .then(response => response.json())
+    .then((data) => {
+      // console.log(data)
+      data.Search.forEach((result) => {
+          const movieList = `<li class="list-inline-item">
+            <img src="${result.Poster}" alt="">
+            <p>${result.Title}</p>
+            </li>`;
+            // console.log(movieList)
 
-        results.insertAdjacentHTML("beforeend", movieList);
-      });;
-    });
-};
+          results.insertAdjacentHTML("beforeend", movieList);
+        });;
+      });
+  };
 
-const form = document.querySelector('#search-movies');
-form.addEventListener('submit', (event) => {
-  event.preventDefault(); // <-- to prevent <form>'s native behaviour
-  const input = event.currentTarget.querySelector('.form-control');
-  results.innerHTML = '';
-  searchMovies(input.value);
+  const form = document.querySelector("#search-movies");
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = event.currentTarget.querySelector('.form-control');
+    results.innerHTML = '';
+    searchMovies(input.value);
+  });
 });
